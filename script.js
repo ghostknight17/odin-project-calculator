b000d103d405678da8be9ad34faec7cc69259769
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-04-14 22:40:23 -0300

    Create JS file with first basic functions and variables

---
+++ script.js
@@ -0,0 +1,23 @@
+let operator;
+let firstNumber;
+let secondNumber;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+  return operator(firstNumber, secondNumber);
+}
