843ae9f54bc0f91453935e7ada3779448c2202cf
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-04-18 00:29:59 -0300

    Get the calculator functionality on the first operation
    Added class names to the number and operator buttons. Initialize
    operator, firstNumber and secondNumber as empty strings. Added event
    listeners to the number buttons to update the screen and the value to
    their variables, to the operator also and to the equals button, for
    calling the operate() function.

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
-let operator;
-let firstNumber;
-let secondNumber;
+let operator = '';
+let firstNumber = '';
+let secondNumber = '';
+
+const screen = document.querySelector('.screen');
+const equalsButton = document.querySelector('#equals-button');
+const numberButtons = document.querySelectorAll(
+  '.buttons-container > .number-button'
+);
+const operatorButtons = document.querySelectorAll(
+  '.buttons-container > .operator-button'
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    if (operator !== '') secondNumber += event.target.innerText;
+    screen.textContent += event.target.innerText;
+  });
+});
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    firstNumber = screen.textContent;
+    operator = event.target.innerText;
+    screen.textContent += operator;
+  });
+});
+equalsButton.addEventListener('click', () => {
+  operate(operator, firstNumber, secondNumber);
+});
 
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
-
+function exponentiate(a, b) {
+  return a ** b;
+}
+function getRemainder(a, b) {
+  return a % b;
+}
 function operate(operator, firstNumber, secondNumber) {
-  return operator(firstNumber, secondNumber);
+  let result;
+  if (operator === '+') {
+    result = add(Number(firstNumber), Number(secondNumber));
+    screen.textContent = result;
+  } else if (operator === '-') {
+    result = subtract(Number(firstNumber), Number(secondNumber));
+    screen.textContent = result;
+  } else if (operator === '*') {
+    result = multiply(Number(firstNumber), Number(secondNumber));
+    screen.textContent = result;
+  } else if (operator === '/') {
+    result = divide(Number(firstNumber), Number(secondNumber));
+    screen.textContent = result;
+  } else if (operator === '**') {
+    result = exponentiate(Number(firstNumber), Number(secondNumber));
+    screen.textContent = result;
+  } else {
+    result = getRemainder(Number(firstNumber), Number(secondNumber));
+    screen.textContent = result;
+  }
 }
